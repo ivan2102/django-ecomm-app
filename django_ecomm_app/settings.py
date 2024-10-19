424cbf21fb8d96c74a39346c25caac68dabde5e9
Ivan Radisavljevic <50371798+ivan2102@users.noreply.github.com>
2024-10-19 21:45:00 +0200

    Update settings.py

--- django_ecomm_app/settings.py
+++ django_ecomm_app/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-ecomm-app.onrender.com', '*']
 
-#CSRF_TRUSTED_ORIGINS = ['']
+CSRF_TRUSTED_ORIGINS = ['https://django-ecomm-app.onrender.com']
 
 
 # Application definition
